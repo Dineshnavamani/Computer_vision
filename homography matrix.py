fe4b4a75381a747b6f3c49b834901d6c9a5b1d59
Dinesh N <112761051+Dineshnavamani@users.noreply.github.com>
2023-10-18 13:13:16 +0530

    Update homography matrix.py

--- homography matrix.py
+++ homography matrix.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
-im_src = cv2.imread("C:/Users/manvitha/OneDrive/Pictures/manvitha.jpg")
+im_src = cv2.imread("alone.jpeg")
 pts_src = np.array([[141, 131], [480, 159], [493, 630],[64, 601]])
-im_dst = cv2.imread("C:/Users/manvitha/OneDrive/Pictures/manvitha.jpg")
+im_dst = cv2.imread("nature.jpeg")
 pts_dst = np.array([[318, 256],[534, 372],[316, 670],[73, 473]])
 h, status = cv2.findHomography(pts_src, pts_dst)
 im_out = cv2.warpPerspective(im_src, h, (im_dst.shape[1],im_dst.shape[0]))
